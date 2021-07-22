4fec43fb192dcfdf72cacbdbc19934ff43131924
Aina Perez <aina.perez.1996@gmail.com>
2021-07-22 22:37:19 +0200

    Cash register final exercices for FreeCodeCamp Javascript course

--- Javascript_algorithms_and_data_structures/cash_register.js
+++ Javascript_algorithms_and_data_structures/cash_register.js
@@ -1,8 +1,9 @@
 function checkCashRegister(price, cash, cid) {
     var difference = cash - price;
+    const originalDiff = difference;
     cid.reverse();
-    var cidSum = 0;
     
+    var cidSum = 0;
     for(let i = 0; i<cid.length; i++){
       cidSum += cid[i][1];
     }
@@ -13,41 +14,60 @@ function checkCashRegister(price, cash, cid) {
       ["TEN", 10], 
       ["FIVE", 5], 
       ["ONE", 1], 
-      ["QUARTER", 0.25], 
+      ["QUARTER", 0.25],
+      ["DIME", 0.1],
       ["NICKEL", 0.05],
-      ["PENNY", 0.01],
-      ["DIME", 0.1] 
+      ["PENNY", 0.01]
       ]
+      
     var result = [...arrCurrency];//new array where we are going to include all coins to return(it is going to be the value of the key 'change' 
     
     for(let i = 0; i<arrCurrency.length; i++){
       let returnMoney = 0; //initialize variable where we are going to save each coin value to return.
       let bill = cid[i][1]/arrCurrency[i][1]//check if there is a bill or coin available (has to be greater than 1)
-        while(difference>=arrCurrency[i][1] && bill>=1){
+        bill.toFixed(2);
+        while(difference.toFixed(2)>=arrCurrency[i][1] && bill>=1){
           difference -= arrCurrency[i][1];
           returnMoney += arrCurrency[i][1];
+          bill--;
           
         }
-          result[i][1]= returnMoney;
+          if(returnMoney>0){
+            if(returnMoney - Math.floor(returnMoney) !== 0){result[i][1]= returnMoney.toFixed(2)
+            result[i][1] = parseFloat(result[i][1])}else{
+              result[i][1]= returnMoney;
+            }
+            
+          }else{
+            result[i][1]= returnMoney;
+          }
     }
-    let resultFiltered =[];
     
-    for(let a = 0; a<result.length; a++){
-      if(result[a][1]!==0){
-        result.filtered.push(result[a][1]);  
-      } 
-      }
-    console.log(resultFiltered);
+    let sumResult = 0;//In order to check if we can return the exact change we create this variable where we will store the sum of all the change calculated before. If the sum is less to the change, then it will mean we can not give back the exact change.
+    for(let i = 0; i<cid.length; i++){
+      sumResult += result[i][1];
+    }
+    sumResult = sumResult.toFixed(2);
     
     
-    
-      if(cidSum < difference){
+    cid.reverse();//we reverse the cid again to return it to the original.
+    console.log(sumResult)
+    console.log(originalDiff)
+    if(cidSum < originalDiff || sumResult < originalDiff  ){
       return {status: 'INSUFFICIENT_FUNDS', change: []};
-      }else if(cidSum = difference){
-        return {status: 'CLOSED', change: [cid]}
+      }else if(cidSum == originalDiff ){
+        return {status: 'CLOSED', change: cid}
       }else{
-        return {status: 'OPEN', change: [cid]}
+        let resultFiltered =[];
+        for(let a = 0; a<result.length; a++){
+          if(result[a][1]!==0){
+            resultFiltered.push(result[a]);  
+          } 
+          }
+        return {status: 'OPEN', change: resultFiltered}
       }
     }
     
-    checkCashRegister(19.5, 20, [["PENNY", 1.01], ["NICKEL", 2.05], ["DIME", 3.1], ["QUARTER", 4.25], ["ONE", 90], ["FIVE", 55], ["TEN", 20], ["TWENTY", 60], ["ONE HUNDRED", 100]]);
+    
+    
+    console.log(checkCashRegister(19.5, 20, [["PENNY", 0.01], ["NICKEL", 0], ["DIME", 0], ["QUARTER", 0], ["ONE", 1], ["FIVE", 0], ["TEN", 0], ["TWENTY", 0], ["ONE HUNDRED", 0]]));
