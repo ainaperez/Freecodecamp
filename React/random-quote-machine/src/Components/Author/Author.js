84a243675516bdaac5db34568903a25e243a8c82
Aina Perez <aina.perez.1996@gmail.com>
2021-09-06 13:23:58 +0200

    change file structure

--- React/random-quote-machine/src/Components/Author/Author.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'; 
-
-const author = (props) => (
-    
-    <p id='author'>-{props.author}</p>
-    
-)
-
-export default author;
--- React/random-quote-machine/src/Components/NewQuote/NewQuote.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'; 
-import classes from './NewQuote.module.css';
-
-const newQuote = (props) => (
-    <button id='new-quote' className={classes.newQuote} onClick={props.clicked}>New quote</button>
-)
-
-export default newQuote;
